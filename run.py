039b70d221064a96893b64143879080e0e54811f
John Murray <jmurray1989@hotmail.com>
2020-06-15 20:40:46 +0000

    added skoot button to homepage and set up nav links

--- run.py
+++ run.py
@@ -1,13 +1,30 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request, url_for
 
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def base():
-    return render_template('base.html')
+@app.route('/index')
+def index():
+    """Home page render"""
+    return render_template('index.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/skoot')
+def skoot():
+    return render_template('skootroute.html')
+
+
+@app.route('/skoot')
+def skoot_button():
+    return redirect(url_for('skoot'))
 
 
 if __name__ == '__main__':
