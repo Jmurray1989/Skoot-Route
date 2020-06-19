682f0071f01f4b513329a4c0d4a267aa65a026ba
John Murray <jmurray1989@hotmail.com>
2020-06-19 18:08:41 +0000

    connection made with MongoDB using enviroment variables

--- run.py
+++ run.py
@@ -1,12 +1,15 @@
 import os
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
-from bson.objectid import ObjectId 
+from bson.objectid import ObjectId
+from os import path
+if path.exists("env.py"):
+    import env
 
 
 app = Flask(__name__)
-app.config["MONGO_DBNAME"] = os.getenv('MONGO_DBNAME', 'mongodb://localhost')
-app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
+app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 
 
 mongo = PyMongo(app)
@@ -25,21 +28,15 @@ def about():
     return render_template('about.html')
 
 
-@app.route('/skootroutes')
-def skoot_routes():
-    return render_template("skootroute.html", my_routes=mongo.db.my_routes.find())
+@app.route('/routes')
+def routes():
+    return render_template("routes.html", routes=mongo.db.routes.find())
 
 
 @app.route('/mobile')
 def mobile():
     """Mobile page render"""
     return render_template('mobile.html')
-
-
-@app.route('/skootroutes')
-def skoot_button():
-    """Button re-direct on homepage"""
-    return redirect(url_for('skootroutes'))
 
 
 if __name__ == '__main__':
